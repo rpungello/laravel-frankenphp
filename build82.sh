6d91a0d2ae3c91e11dbbdf8586d886f99a3e44c2
Rob Pungello <rob.pungello@gmail.com>
2025-07-15 18:36:51 -0400

    Update image tagging

--- build82.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker buildx build --pull --push --build-arg PHP_VERSION=8.2 --platform=linux/amd64,linux/arm64/v8 --tag rpungello/laravel-franken:8.2 --tag 692057070962.dkr.ecr.us-east-1.amazonaws.com/laravel-franken:8.2 .
--- build84.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker buildx build --pull --push --build-arg FRANKEN_VERSION=1.8 --build-arg PHP_VERSION=8.4 --platform=linux/amd64,linux/arm64/v8 --tag rpungello/laravel-franken:8.4 --tag rpungello/laravel-franken:latest --tag 692057070962.dkr.ecr.us-east-1.amazonaws.com/laravel-franken:8.4 --tag 692057070962.dkr.ecr.us-east-1.amazonaws.com/laravel-franken:latest .
